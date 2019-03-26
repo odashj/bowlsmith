2a179ba2f958ab701031486bcfca47c1e02214ea
odashj <olivia@o-j.co>
2019-03-26 07:44:13 -0400

    Fixed typo in footer

--- footer.php
+++ footer.php
@@ -3,11 +3,10 @@
 		<div class="footer">
 			<h2 class="three columns darkBrown">Jesse Smith,<br>craftsman
 			</h2>
-			<p class="footer-text three columns">503.708.4916 
-				<br>jesse@bowlsmith.com
+			<p class="footer-text three columns">503.708.4916 <br>jesse@bowlsmith.com
 			</p>
 			<p class="footer-text three columns">2753 SW Sherwood Drive 
-				<br>Portland, Oregon 9720
+				<br>Portland, Oregon 97201
 			</p>
 			<p class="footer-text three columns">Copyright ©2019 Jesse Smith,
 				<br>All Rights Reserved.
