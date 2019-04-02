3d4851b7a707a66c1eec0108da73114fc9dff30d
Olivia Johnson <olivia@o-j.co>
2019-04-01 20:15:10 -0400

    Updated about text

--- about.php
+++ about.php
@@ -9,7 +9,7 @@ include "nav.php";
 	<div class="container content">
 		<div class="eight columns offset-by-two">
 			<div class="headline">
-				<h1 class="darkBrown">Behind the Bowls</h1>
+				<h1 class="darkBrown">From the Craftsman</h1>
 			</div>
 		</div>
 		<div class="ten columns offset-by-one">
@@ -17,8 +17,12 @@ include "nav.php";
 		</div>
 		<div class="eight columns offset-by-two">
 			<div class="bio">
-				<!-- <h4>As an independent financial advisor, I provide sound and cost-effective consulting solutions to small businesses, non-profits, community development organizations and local governments. I am entrepreneurial and resourceful, working with clients to overcome shared challenges.</h4>
-				<h4>I have 30 years experience helping organizations succeed in business and real estate development by securing financing, optimizing investment, evaluating projects, and reducing debt load.</h4> -->
+				<h4>Throughout my professional career, I have always fed my soul through craftsmanship. Years as a silversmith transitioned to carpentry;  carpentry transitioned to fine furniture. I now turn wooden bowls designed with a purpose: working wood. I try to balance artistry and purpose—amplifying the beauty of the wood I am turning and still encouraging its everyday use.</h4>
+				<h4>I use a bowl's final shape, its weight and size—its tactile qualities—to complement artistry and function.</h4>
+				<h4>I think of turning now as art in motion—art at 1000 revolutions per minute you might say—with each bowl as a unique and distinctive finished piece available to use and enjoy.</h4>
+				<h4>I am a member of the <a href="https://guildoforegonwoodworkers.org/" target="_blank">Guild of Oregon Woodworkers</a>, <a href="https://www.northwestwoodturners.com/" target="_blank">Northwest Woodturners</a> and the <a href="https://www.woodturner.org/" target="_blank">American Association of Woodturners</a>. Each is a great group of men and women encouraging a craftsman to master their craft.</h4>
+				<h4>If you are interested in purchasing a bowl from my current inventory or if you have a specific design/production request, please contact me by email or phone/text. My current inventory, as you can see from the gallery photos, offers a diverse selection of wood species, sizes and shapes. As to special orders, I am happy to work with your particular interests and offer suggestions where I can.</h4>
+				<h4>I can also let you know the date and location of my next fair.</h4>
 			</div>
 		</div>
 	</div>
